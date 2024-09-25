92ba29904909eba3d5d35a9aa39e9947a23519ab
choi-ys <project.log.062@gmail.com>
2024-09-25 20:01:43 +0900

    chore: code polishing

--- support/swagger/build.gradle.kts
+++ support/swagger/build.gradle.kts
@@ -1,10 +1,7 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 tasks.withType<Jar> { enabled = true }
-
-tasks.named<BootJar>("bootJar") {
-    enabled = false
-}
+tasks.named<BootJar>("bootJar") { enabled = false }
 
 dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
--- support/web/build.gradle.kts
+++ support/web/build.gradle.kts
@@ -1,10 +1,8 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 tasks.withType<Jar> { enabled = true }
+tasks.named<BootJar>("bootJar") { enabled = false }
 
-tasks.named<BootJar>("bootJar") {
-    enabled = false
-}
 dependencies {
     api("org.springframework.boot:spring-boot-starter-web")
 }
